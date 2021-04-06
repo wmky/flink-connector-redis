c74eacfff0692f817e49b53d0e94c6f9f23f892a
youck <youck@leyoujia.com>
2021-04-07 07:46:33 +0800

    fix: 修复校验redis primary key的问题

--- src/main/java/org/apache/flink/streaming/connectors/redis/table/RedisDynamicTableSink.java
+++ src/main/java/org/apache/flink/streaming/connectors/redis/table/RedisDynamicTableSink.java
@@ -44,7 +44,7 @@ public class RedisDynamicTableSink implements DynamicTableSink {
 
     @Override
     public ChangelogMode getChangelogMode(ChangelogMode requestedMode) {
-        validatePrimaryKey(requestedMode);
+//        validatePrimaryKey(requestedMode);
         return ChangelogMode.newBuilder()
                 .addContainedKind(RowKind.INSERT)
                 .addContainedKind(RowKind.DELETE)
